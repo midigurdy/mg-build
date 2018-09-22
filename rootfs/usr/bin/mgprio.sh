71878ef346894f2b3451a0e3ec5b504654be6db0
Marcus Weseloh <marcus@weseloh.cc>
2018-09-22 22:33:04 +0200

    Fine tune irq priorities: I2C can wait, ktimersoftd should be above mgurdy

--- rootfs/usr/bin/mgprio.sh
+++ rootfs/usr/bin/mgprio.sh
@@ -16,14 +16,17 @@ function set_priorities() {
     prio 1 85 "1c25000.mgurdy_keys" "irq"
 
     # make sure timer interrupts are serviced before all "normal" irqs
-    prio - 51 "ktimersoftd/0"     "proc"
-    prio - 51 "ktimersoftd/1"     "proc"
+    prio - 52 "ktimersoftd/0"     "proc"
+    prio - 52 "ktimersoftd/1"     "proc"
 
     # Fix disk access and usb controller to fist CPU, as fluidsynth
     # and MidiGurdy UI and modelling runs on second CPU
     prio 1 50 "mmcqd/0"           "proc"
     prio 1 50 "sunxi-mmc"         "irq"
     prio 1 50 "musb-hdrc.1.auto"  "irq"
+
+    # I2C controller can wait a little
+    prio - 49 "mv64xxx_i2c" "irq"
 }
 
 set_priorities
