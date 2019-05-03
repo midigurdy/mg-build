30f816bd604971c262c61c23baa91202f2d0642b
Marcus Weseloh <marcus@weseloh.cc>
2019-05-03 11:31:08 +0200

    Upgrade to latest FluidSynth version

--- package/fluidsynth/fluidsynth.mk
+++ package/fluidsynth/fluidsynth.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FLUIDSYNTH_VERSION = v2.0.4
+FLUIDSYNTH_VERSION = v2.0.5
 FLUIDSYNTH_SITE = $(call github,FluidSynth,fluidsynth,$(FLUIDSYNTH_VERSION))
 FLUIDSYNTH_INSTALL_STAGING = YES
 FLUIDSYNTH_INSTALL_TARGET = YES
