8d20919ec745e939b4d4d327ad518c19cc4dc89a
Marcus Weseloh <marcus@weseloh.cc>
2019-05-09 17:15:09 +0200

    Make use of new tiny-release make command for mg-docs
    Also fixes broken images and static files on singlehtml version

--- package/mgdocs/mgdocs.mk
+++ package/mgdocs/mgdocs.mk
@@ -3,32 +3,19 @@
 # The MidiGurdy documentation
 #
 ################################################################################
-MGDOCS_VERSION = fa7bade4e576128c7e16a475183b08c2174b7691
+MGDOCS_VERSION = 0791e41570ce78266588e1a27b52e4b51508c642
 MGDOCS_SITE = $(call github,midigurdy,mg-docs,$(MGDOCS_VERSION))
 MGDOCS_INSTALL_STAGING = NO
 MGDOCS_INSTALL_TARGET = YES
 MGDOCS_AUTORECONF = NO
 
 define MGDOCS_BUILD_CMDS
-    $(MAKE) -C $(@D) html singlehtml epub latexpdf
+    $(MAKE) -C $(@D) tiny-release
 endef
 
 define MGDOCS_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/srv/www/manual/en/html
-	mkdir -p $(TARGET_DIR)/srv/www/manual/en/singlehtml
-	mkdir -p $(TARGET_DIR)/srv/www/manual/en/download
-    cp -r $(@D)/build/html/* $(TARGET_DIR)/srv/www/manual/en/html/
-    cp -r $(@D)/build/singlehtml/* $(TARGET_DIR)/srv/www/manual/en/singlehtml/
-    cp -r $(@D)/build/latex/midigurdy_manual.pdf $(TARGET_DIR)/srv/www/manual/en/download/
-    cp -r $(@D)/build/epub/midigurdy_manual.epub $(TARGET_DIR)/srv/www/manual/en/download/
-    find $(TARGET_DIR)/srv/www/manual/en/ -wholename "**/fonts/**.ttf" -delete
-    find $(TARGET_DIR)/srv/www/manual/en/ -wholename "**/fonts/**.eot" -delete
-    find $(TARGET_DIR)/srv/www/manual/en/ -wholename "**/fonts/**.svg" -delete
-    find $(TARGET_DIR)/srv/www/manual/en/ -wholename "**/fonts/**.woff2" -delete
-    rm -rf $(TARGET_DIR)/srv/www/manual/en/singlehtml/_static/
-    rm -rf $(TARGET_DIR)/srv/www/manual/en/singlehtml/_images/
-    ln -s ../html/_static $(TARGET_DIR)/srv/www/manual/en/html/_static
-    ln -s ../html/_images $(TARGET_DIR)/srv/www/manual/en/html/_images
+	mkdir -p $(TARGET_DIR)/srv/www/manual/
+    cp -r $(@D)/release/* $(TARGET_DIR)/srv/www/manual/
 endef
 
 $(eval $(generic-package))
